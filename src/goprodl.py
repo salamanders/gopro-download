215c9399d0829df8146e66ce681ccdc792a7a8fe
Benjamin Hill <benjaminhill@gmail.com>
2013-06-11 11:13:41 -0700

    future

--- src/goprodl.py
+++ src/goprodl.py
@@ -4,6 +4,8 @@ Created on Jun 10, 2013
 @author: Benjamin Hill
 '''
 
+from __future__ import absolute_import, division, generators, nested_scopes, print_function, unicode_literals, with_statement
+
 import urllib2
 from bs4 import BeautifulSoup
 import urlparse
@@ -16,22 +18,22 @@ soup = BeautifulSoup(content)
 
 media_re = re.compile(r'(jpg|jpeg|mp4)$', re.IGNORECASE)
 home_image_dir = os.path.join(os.path.expanduser("~"),'Pictures', 'gopro')
-print home_image_dir
+print("Home directory:",home_image_dir)
 
 try:
     os.makedirs(home_image_dir)
-    print "Created:",home_image_dir
+    print("Created:",home_image_dir)
 except (OSError):
-    print 'Already exists:',home_image_dir
+    print('Already exists:',home_image_dir)
 
 for a in soup.findAll('a', attrs={'href': media_re}):
-    print "Found the URL:", a['href']
+    print("Found the URL:", a['href'])
     req = urllib2.urlopen(urlparse.urljoin(base_url, a['href']))
     file_name = os.path.join(home_image_dir, a['href'].split('/')[-1])
     if(os.path.isfile(file_name)):
-        print 'Already exists:',file_name
+        print('Already exists:',file_name)
     else:
-        print "Downloading to:", file_name
+        print("Downloading to:", file_name)
         with open(file_name, 'wb') as fp:
             shutil.copyfileobj(req, fp)
 
